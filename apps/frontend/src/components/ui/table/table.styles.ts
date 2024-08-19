eeaada309eb3e76dc327ef68e7c3d97d25e8bf74
Natael Bernardo <natael.bernardo@hotmail.com>
2024-08-19 22:16:08 +0200

    feat: use rounded corners in table

--- apps/frontend/src/components/ui/table/table.styles.ts
+++ apps/frontend/src/components/ui/table/table.styles.ts
@@ -1,22 +1,35 @@
 import styled from 'styled-components';
 
 export const Table = styled.table`
-  border-collapse: collapse;
+  border-collapse: separate;
+  border-spacing: 0;
+  border: 1px solid ${({ theme }) => theme.colors.primary.main};
+  border-radius: ${({ theme }) => theme.radius.sm};
+  overflow: hidden;
 `;
 
-export const TableRow = styled.tr`
-  border: 1px solid ${({ theme }) => theme.colors.primary.main};
-  border-radius: ${({ theme }) => theme.radius.md};
-`;
+export const TableRow = styled.tr``;
 
 export const TableHeader = styled.th`
   padding: ${({ theme }) => theme.spacing.sm};
   font-weight: bold;
+  text-align: center;
+  border-left: 1px solid ${({ theme }) => theme.colors.primary.main};
+
+  &:first-child {
+    border-left: none;
+  }
 `;
 
 export const TableCell = styled.td`
   padding: ${({ theme }) => theme.spacing.sm};
   text-align: center;
+  border-top: 1px solid ${({ theme }) => theme.colors.primary.main};
+  border-left: 1px solid ${({ theme }) => theme.colors.primary.main};
+
+  &:first-child {
+    border-left: none;
+  }
 `;
 
 export const TableCaption = styled.caption`
